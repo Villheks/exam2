37f323f71bf61eaecd5e1ce5fd3887ec19e428fe
Ane Birgitte Haug Skoglund <aneskoglund@Anes-MacBook-Air.local>
2024-02-20 17:58:34 +0100

    refactored template

--- Program.cs
+++ Program.cs
@@ -1,27 +1,52 @@
-﻿using HTTPUtils;
-using System.Text.Json;
+﻿using Colors = AnsiTools.ANSICodes.Colors;
 using AnsiTools;
-using Colors = AnsiTools.ANSICodes.Colors;
+using HTTPUtils;
+using System.Text.Json;
+using System.Runtime.CompilerServices;
 
-Console.Clear();
-Console.WriteLine("Starting Assignment 2");
 
-// SETUP 
-const string myPersonalID = ""; // GET YOUR PERSONAL ID FROM THE ASSIGNMENT PAGE https://mm-203-module-2-server.onrender.com/
-const string baseURL = "https://mm-203-module-2-server.onrender.com/";
-const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
-const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
 
-// Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
-HttpUtils httpUtils = HttpUtils.instance;
+public class Exam2
+{
 
-//#### REGISTRATION
-// We start by registering and getting the first task
-Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
-Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
-string taskID = ""; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+    const string myPersonalID = "84a06f57d16a4b3388fcbd499ce3c2ee87a1a9827836c2bf75f0a27674621d72"; 
+    const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+    const string startEndpoint = "start/"; 
+    const string taskEndpoint = "task/";
+    string taskID = "kuTw53L";
 
-//#### FIRST TASK 
-// Fetch the details of the task from the server.
-Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
-Console.WriteLine(task1Response);
+     
+    
+    public async Task GetTask()
+    {
+        Console.Clear();
+        Console.WriteLine("Starting Assignment 2");
+        HttpUtils httpUtils = HttpUtils.instance;
+        Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+        Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); 
+         
+    }
+
+    public async Task taskOne()
+    {
+        //#### FIRST TASK 
+        HttpUtils httpUtils = HttpUtils.instance;
+
+        Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); 
+        Console.WriteLine(task1Response);
+    }
+
+    public static async Task Main(string[] args)
+        {
+            Exam2 exam2 = new Exam2();
+            await exam2.GetTask();
+            await exam2.taskOne(); 
+        }
+
+
+};
+   
+
+
+
+
