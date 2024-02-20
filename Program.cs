e9cc2e51ece15ad666cc8de57b1b621e96f68c6b
Ane Birgitte Haug Skoglund <aneskoglund@Anes-MacBook-Air.local>
2024-02-20 18:51:43 +0100

    Task One completed

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using HTTPUtils;
 using System.Text.Json;
 using System.Runtime.CompilerServices;
 
-
-
 public class Exam2
 {
 
@@ -24,16 +22,43 @@ public class Exam2
         HttpUtils httpUtils = HttpUtils.instance;
         Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
         Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); 
-         
+        Response taskResponse = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); 
+        TaskDetails taskDetails = JsonSerializer.Deserialize<TaskDetails>(taskResponse.content);
+
+        PrintTaskDetails(taskDetails);
+        
     }
 
     public async Task taskOne()
     {
-        //#### FIRST TASK 
         HttpUtils httpUtils = HttpUtils.instance;
+        Response taskResponse = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
+        TaskDetails taskDetails = JsonSerializer.Deserialize<TaskDetails>(taskResponse.content);
 
-        Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); 
-        Console.WriteLine(task1Response);
+        string[] parts = taskDetails.parameters.Split(',');
+        List<int> primeNumbers = new List<int>();
+
+        foreach (string part in parts)
+        {
+            if (int.TryParse(part, out int number))
+            {
+                if (IsPrime(number))
+                {
+                    primeNumbers.Add(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid number: {part}");
+            }
+        }
+
+        primeNumbers.Sort();
+
+        string answer = string.Join(",", primeNumbers);
+
+        Response taskAnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer);
+        Console.WriteLine($"Answer: {Colors.Green}{taskAnswerResponse}{ANSICodes.Reset}");
     }
 
     public static async Task Main(string[] args)
@@ -42,10 +67,54 @@ public class Exam2
             await exam2.GetTask();
             await exam2.taskOne(); 
         }
+    static void PrintTaskDetails(TaskDetails taskDetails)
+        {
+            Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{taskDetails.title}{ANSICodes.Reset}\n{taskDetails.description}\nParameters: {Colors.Yellow}{taskDetails.parameters}{ANSICodes.Reset}");
+            Console.WriteLine($"Task ID: {taskDetails.taskID}");
+            Console.WriteLine($"User ID: {taskDetails.usierID}");
+            
+        }
+    static bool IsPrime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
 
+            if (num <= 3)
+            {
+                return true;
+            }
+
+            if (num % 2 == 0 || num % 3 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    class TaskDetails
+    {
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public string? taskID { get; set; }
+        public string? usierID { get; set; }
+        public string? parameters { get; set; }
+
+        
+    }
 
 };
-   
+
 
 
 
